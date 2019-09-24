4e8124cae44b49559deba1bc0edd46d1e98162a3
Luis Carlos de Souza <luis.muniz@gett.com.br>
2019-09-24 13:57:40 -0300

    Updates

---
+++ precache-manifest.d38b70dbe163ae9b85fd2ca19ae3941b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8072424d3e338d954ae2db983955fe53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80209b30806e100ef02b",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "8c012c6f9d209f1eaaf8",
+    "url": "/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "80209b30806e100ef02b",
+    "url": "/static/js/2.a0218c95.chunk.js"
+  },
+  {
+    "revision": "8c012c6f9d209f1eaaf8",
+    "url": "/static/js/main.623babe8.chunk.js"
+  },
+  {
+    "revision": "51ddb1d5232cc400a410",
+    "url": "/static/js/runtime~main.c260b7c7.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/template-valandil/precache-manifest.70d4f0a5ba8f9d0219f04d05987f45fd.js"
+  "/precache-manifest.d38b70dbe163ae9b85fd2ca19ae3941b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/template-valandil/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
