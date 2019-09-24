761947d232dfed7ed5266a8c2e9b9913a9d33c5f
Luis Carlos de Souza <luis.muniz@gett.com.br>
2019-09-24 11:10:36 -0300

    Updates

---
+++ precache-manifest.1a25017e32fbbbe3df6f79d42b93b74c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311e33beabe1b3d9d93d50831c221d08",
+    "url": "/template-valandil/dashboard/index.html"
+  },
+  {
+    "revision": "80209b30806e100ef02b",
+    "url": "/template-valandil/dashboard/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "5f2fcac6c521639eecc8",
+    "url": "/template-valandil/dashboard/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "80209b30806e100ef02b",
+    "url": "/template-valandil/dashboard/static/js/2.a0218c95.chunk.js"
+  },
+  {
+    "revision": "5f2fcac6c521639eecc8",
+    "url": "/template-valandil/dashboard/static/js/main.ac1e7011.chunk.js"
+  },
+  {
+    "revision": "9ea2a3d9d0b23ac74bef",
+    "url": "/template-valandil/dashboard/static/js/runtime~main.c7d92767.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/template-valandil/dashboard/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/template-valandil/dashboard/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/template-valandil/dashboard/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/template-valandil/dashboard/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/template-valandil/dashboard/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
